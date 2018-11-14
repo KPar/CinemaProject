4c3f7035e1fec326c2498552a2e9ba3bfcf9c0f4
ivan <ilee.4123@gmail.com>
2018-11-13 21:08:27 -0500

    - Added Sign-out button to Admin Gui

--- src/application/AdminController.java
+++ src/application/AdminController.java
@@ -1,4 +1,24 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminController {
+
+        public void Public(ActionEvent event) throws IOException {
+            Parent account = FXMLLoader.load(getClass().getResource("Public.fxml"));
+            Scene accountscene = new Scene(account);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            window.setScene(accountscene);
+            window.show();
+
+        }
 }
