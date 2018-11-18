e3d650de1ab06bef99112ec9ba624e983e64e0bb
ivan <ilee.4123@gmail.com>
2018-11-18 01:55:46 -0500

    - Added methods and checks for radiobuttons when adding a cinema in Admin GUI. Radio buttons respond whether some or all buttons are selected. - Admins are able to add cinemas.

--- src/application/AdminController.java
+++ src/application/AdminController.java
@@ -3,6 +3,7 @@ package application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
@@ -12,17 +13,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javax.swing.*;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
 public class AdminController {
+        DatabaseHelper dbHelper;
 
         @FXML
         private TextField movietitle;
@@ -79,6 +84,7 @@ public class AdminController {
         daytime.getItems().addAll("am","pm");
         movieS.getItems().addAll("movies in database");
         cinemaS.getItems().addAll("cinemas in database");
+        dbHelper=new DatabaseHelper();
 
         x_address.textProperty().addListener(new ChangeListener<String>() {
             @Override
@@ -98,6 +104,82 @@ public class AdminController {
             }
         } );
 
+        All.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                All.setSelected(true);
+                if (G.isSelected()) {
+                    G.setSelected(false);
+                }
+                if (PG.isSelected()) {
+                    PG.setSelected(false);
+                }
+                if (PG13.isSelected()) {
+                    PG13.setSelected(false);
+                }
+                if (R.isSelected()) {
+                    R.setSelected(false);
+                }
+                if (NC17.isSelected()) {
+                    NC17.setSelected(false);
+                }
+            }
+        });
+
+        G.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!G.isSelected() && !PG.isSelected() && !PG13.isSelected() && !R.isSelected() && !NC17.isSelected()){
+                    All.setSelected(true);
+                }
+                else
+                    All.setSelected(false);
+            }
+        });
+
+        PG.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!G.isSelected() && !PG.isSelected() && !PG13.isSelected() && !R.isSelected() && !NC17.isSelected()){
+                    All.setSelected(true);
+                }
+                else
+                    All.setSelected(false);
+            }
+        });
+
+        PG13.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!G.isSelected() && !PG.isSelected() && !PG13.isSelected() && !R.isSelected() && !NC17.isSelected()){
+                    All.setSelected(true);
+                }
+                else
+                    All.setSelected(false);
+            }
+        });
+
+        R.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!G.isSelected() && !PG.isSelected() && !PG13.isSelected() && !R.isSelected() && !NC17.isSelected()){
+                    All.setSelected(true);
+                }
+                else
+                    All.setSelected(false);
+            }
+        });
+
+        NC17.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(!G.isSelected() && !PG.isSelected() && !PG13.isSelected() && !R.isSelected() && !NC17.isSelected()){
+                    All.setSelected(true);
+                }
+                else
+                    All.setSelected(false);
+            }
+        });
     }
 
         public void Public(ActionEvent event) throws IOException {
@@ -124,17 +206,40 @@ public class AdminController {
                 AlertBox("Movie added", "Movie added");
         }
 
-        public void cinemaApply(ActionEvent e){
-            if(cinemaname.getText().isEmpty()){
-                AlertBox("cinema", "cinema field is empty.");
+        public void cinemaApply(ActionEvent e) {
+            if (cinemaname.getText().isEmpty()) {
+                AlertBox("Cinema", "cinema field is empty.");
+            }
+            else if (x_address.getText().isEmpty() || y_address.getText().isEmpty()) {
+                AlertBox("Address", "address field is incomplete.");
             }
 
-            else if(x_address.getText().isEmpty() || y_address.getText().isEmpty()){
-                AlertBox("address", "address field is incomplete.");
+            else if (G.isSelected() || PG.isSelected() || PG13.isSelected() || R.isSelected() || NC17.isSelected()) {
+                ArrayList<String> rating = new ArrayList<String>();
+                if (G.isSelected()) {
+                    rating.add("G");
+                }
+                if (PG.isSelected()) {
+                    rating.add("PG");
+                }
+                if (PG13.isSelected()) {
+                    rating.add("PG13");
+                }
+                if (R.isSelected()) {
+                    rating.add("R");
+                }
+                if (NC17.isSelected()) {
+                    rating.add("NC17");
+                }
+
+                dbHelper.addCinema(cinemaname.getText(), Integer.parseInt(x_address.getText()), Integer.parseInt(y_address.getText()), rating);
+                AlertBox("Cinema Added", "Cinema added to database with " + rating + " restrictions");
             }
 
-            else
-                AlertBox("Cinema added", "Cinema added");
+            else {
+                dbHelper.addCinema(cinemaname.getText(), Integer.parseInt(x_address.getText()), Integer.parseInt(y_address.getText()));
+                AlertBox("Cinema Added", "Cinema added with no restrictions");
+            }
         }
 
         public void showtimeApply(ActionEvent e){
